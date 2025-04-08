20f07381eac62ee9384ed44e5fb1c1b09c6f9dec
Jorge Armando <163047032+Jorge-Armando-Von-Doellinger@users.noreply.github.com>
2025-04-08 12:15:30 -0700

    update - starting streaming service!
    update - starting streaming service!

---
+++ Versions/v0-developing/src/StramingService/OmniSphere.StramingService.Infrastructure/Implementations/Services/StramingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OmniSphere.StramingService.Core.Interfaces.Services;
+
+namespace OmniSphere.StramingService.Infrastructure.Implementations.Services
+{
+    public class StramingService : IStreamingService
+    {
+        public Task StartStreaming(string username, string liveName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopStreaming(string username, string liveName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
